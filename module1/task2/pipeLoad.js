4791a83163f8fd9438bfce27ea8f0a88f5c24404
Mykhailo_Koshelnyk <Mykhailo_Koshelnyk@epam.com>
2020-08-04 01:03:43 +0300

    update task2 with use pipeline methods of stream

--- module1/task2/pipeLoad.js
+++ module1/task2/pipeLoad.js
@@ -1,18 +1,21 @@
 const csv = require('csvtojson');
-const fs = require('fs')
+const fs = require('fs');
+const { pipeline } = require('stream');
 
-const csvFilePath = './task2/csv/example.csv'
-const txtFilePath = './task2/fromCSV.txt'
-
-
-const readStream = fs.createReadStream(csvFilePath, {
-    highWaterMark: 20
-});
-const writeStream = fs.createWriteStream(txtFilePath, 'utf8');
+const csvFilePath = './task2/csv/example.csv';
+const txtFilePath = './task2/fromCSV.txt';
 
 
-try {
-    readStream.pipe(csv()).pipe(writeStream);
-} catch (e) {
-    console.log(`Some error happened: ${e}`)
-}
+pipeline(
+    fs.createReadStream(csvFilePath, {
+        highWaterMark: 20
+    }),
+    csv({output:"json"}),
+    fs.createWriteStream(txtFilePath, 'utf8'),
+    (error) => {
+        if (error) {
+            console.error(`Happened some error: ${error}`);
+        }
+        console.log('Complete success');
+    }
+)
